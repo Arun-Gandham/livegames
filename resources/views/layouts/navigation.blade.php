929adf06bb4eeb84a170e761ddcb79e547635e2b
Gandham Arun sai <125370988+Arun-Gandham@users.noreply.github.com>
2026-02-12 16:44:27 +0530

    Added my love logoo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img style="height: 6rem;margin-top: 10px;" src="{{ asset('images/logo_new.png') }}" alt="Logo" class="block h-9 w-auto" />
+
                     </a>
                 </div>
 
@@ -16,6 +17,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Services') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown or Login -->
